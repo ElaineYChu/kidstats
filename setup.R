5e5af31151a8e824c36bbf36b46a6e2d6bb64ff8
ElaineYChu <elainechu@unr.edu>
2021-07-24 14:49:07 -0700

    Updating files and scripts

---
+++ setup.R
@@ -0,0 +1,19 @@
+##########################################
+##
+##      Script to install required
+##    packages for KidStats (v.0.2.0)
+##
+##########################################
+
+install.packages("shiny")
+install.packages("shinydashboard")
+install.packages("shinyjs")
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("stringr")
+install.packages("devtools")
+
+devtools::install_github("MichaelHoltonPrice/yada")
+
+
+
